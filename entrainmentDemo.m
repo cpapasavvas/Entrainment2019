c10cd95eed5a56da5e5c1fe2ae049f1fb9c0b95a
cpapasavvas <cpapasavva@gmail.com>
2019-06-24 16:56:25 +0100

    Update entrainmentDemo.m

--- entrainmentDemo.m
+++ entrainmentDemo.m
@@ -5,13 +5,10 @@ optimSet = transformSet(optimEndAlt);
 phase = 0;  % initial phase difference; to multiply pi/2
 AMP = 0.7;  % amplitude      
 FREQ = 4;   % frequency
-elegance = 0;   %binary choice of IO function, non-elegant one
-                % was used for the manuscript and the parameter sets were
-                % generated based on elegance = 0
                 
 plotFlag = 1;
 
-q = 0.25;
+q = 0.25;   % divisiveness parameter: ranges from 0 to 1
 parameters = [optimSet q AMP FREQ]; 
 
-[~,syn_index, tConv, absP, delayPh]=solverODEvarInput2(plotFlag, parameters, phase, elegance);
+[~,syn_index, tConv, absP, delayPh]=solverODEvarInput2(plotFlag, parameters, phase, 0);
